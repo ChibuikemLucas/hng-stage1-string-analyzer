82199e3b2cf9b9d0383d745f77de1d3f0e0f7e09
ChibuikemLucas <chibuikemlucas@gmail.com>
2025-10-22 13:11:06 +0100

    feat: Add base route.
    Signed-off-by: ChibuikemLucas <chibuikemlucas@gmail.com>

--- src/app.js
+++ src/app.js
@@ -7,6 +7,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// ✅ Base route 
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: "Welcome to the HNG Stage 1 String Analyzer API 🚀",
+        routes: {
+            analyze: "POST /strings",
+            getString: "GET /strings/:string_value",
+            getAll: "GET /strings",
+            filterByNaturalLanguage: "GET /strings/filter-by-natural-language?query=",
+            delete: "DELETE /strings/:string_value"
+        },
+        author: "Lucas-Emerenini Chibuikem Kennedy"
+    });
+});
+
 // routes
 app.use('/', stringRoutes);
 
